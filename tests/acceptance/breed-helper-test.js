68e4fc54b06744ca591189f77133cdb9f4395d83
aramirezdecruz3148 <leigh3148@gmail.com>
2020-12-22 12:43:03 -0800

    Refactored using components, added component testing, brought back the application.hbs

--- tests/acceptance/breed-helper-test.js
+++ tests/acceptance/breed-helper-test.js
@@ -9,6 +9,8 @@ module('Acceptance | breed helper', function(hooks) {
     await visit('/');
 
     assert.equal(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('🐾 Breeds');
     assert.dom('h2').hasText('Welcome to Breed Helper');
 
     assert.dom('.jumbo a.button').hasText('About Us');
@@ -26,6 +28,8 @@ module('Acceptance | breed helper', function(hooks) {
     await visit('/about');
 
     assert.equal(currentURL(), '/about');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('🐾 Breeds');
     assert.dom('h2').hasText('About Breed Helper');
 
     assert.dom('.jumbo a.button').hasText('Contact Us');
@@ -38,11 +42,31 @@ module('Acceptance | breed helper', function(hooks) {
     await visit('/contact');
 
     assert.equal(currentURL(), '/contact');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('🐾 Breeds');
     assert.dom('h2').hasText('Contact Us');
 
     assert.dom('.jumbo a.button').hasText('About Us');
     await click('.jumbo a.button');
 
     assert.equal(currentURL(), '/about');
+  });
+
+  test('navigating using the nav-bar', async function(assert) {
+    await visit('/');
+
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('🐾 Breeds');
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+
+    await click('nav a.menu-about');
+    assert.equal(currentURL(), '/about');
+
+    await click('nav a.menu-contact');
+    assert.equal(currentURL(), '/contact');
+
+    await click('nav a.menu-index');
+    assert.equal(currentURL(), '/');
   });
 });
