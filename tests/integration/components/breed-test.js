b3e4a1db4ec731bb561547744fb2f381ef58fb86
aramirezdecruz3148 <leigh3148@gmail.com>
2020-12-22 18:45:07 -0800

    Updated data by moving it to a json file and fetched it within the route

--- tests/integration/components/breed-test.js
+++ tests/integration/components/breed-test.js
@@ -7,7 +7,24 @@ module('Integration | Component | breed', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders details about a breed', async function(assert) {
-    await render(hbs`<Breed />`);
+    this.setProperties({
+      breed: {
+        name: 'Great Dane',
+        weight: '110 - 175',
+        height: '28 - 32 inches',
+        origin: 'Germany',
+        location: {
+          lat: 52.5200,
+          lng: 13.4050,
+        },
+        group: 'Working',
+        bedrooms: 15,
+        image: 'assets/images/great-dane.jpg',
+        span: '7 - 10 years',
+      }
+    });
+    
+    await render(hbs `<Breed @breed={{this.breed}} />`);
 
     assert.dom('article').hasClass('breed');
     assert.dom('article h3').hasText('Great Dane');
@@ -16,7 +33,6 @@ module('Integration | Component | breed', function(hooks) {
     assert.dom('article .detail.life-span').includesText('7 - 10 years');
     assert.dom('article .detail.breed-group').includesText('Working');
     assert.dom('article .detail.country-of-origin').includesText('Germany');
-    assert.dom('article .detail.temperament').includesText('Friendly, gentle, loving');
     assert.dom('article .image').exists();
     assert.dom('article .map').exists();
   });
