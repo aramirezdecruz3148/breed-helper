226666abe8555a88a1cc09e450f7bd0e71089e7f
aramirezdecruz3148 <leigh3148@gmail.com>
2020-12-23 18:44:13 -0800

    still working on netlify images

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,7 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  let app = new EmberApp(defaults, {
+  let app = new EmberApp({
     fingerprint: {
       enabled: false
     }
