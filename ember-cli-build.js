606cf6cc2f077f86c4e9820a9dfbb1885aced75b
aramirezdecruz3148 <leigh3148@gmail.com>
2020-12-23 18:48:05 -0800

    One more try

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,10 +3,8 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  let app = new EmberApp({
-    fingerprint: {
-      enabled: false
-    }
+  let app = new EmberApp(defaults, {
+    // Add options here
   });
 
   // Use `app.import` to add additional libraries to the generated
